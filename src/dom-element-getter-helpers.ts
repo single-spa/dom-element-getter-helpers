c548e0346c82dc8d47d868d295307b90c3742c60
Jolyn Denning <joeldenning@gmail.com>
2025-03-04 17:12:58 -0700

    Use exported types from latest single-spa (#8)
    * Use exported types from latest single-spa
    
    * Self review

--- src/dom-element-getter-helpers.ts
+++ src/dom-element-getter-helpers.ts
@@ -1,22 +1,12 @@
-import { registerApplication, mountRootParcel } from "single-spa";
+import { AppProps } from "single-spa";
 
-type CustomProps = Parameters<typeof mountRootParcel>[1];
-type Application<Props extends CustomProps> = Extract<
-  Parameters<typeof registerApplication<Props>>[1],
-  { bootstrap }
->;
-type LifecycleFn<Props extends CustomProps> = Extract<
-  Application<Props>["bootstrap"],
-  (config: any) => Promise<any>
->;
-type AppProps<Props extends CustomProps> = Parameters<LifecycleFn<Props>>[0];
-
-type AllProps<ExtraProps = {}> = AppProps<ExtraProps> & {
-  domElement?: HTMLElement;
-  domElementGetter?(): HTMLElement;
-  // Old versions of single-spa had an appName prop
-  appName?: string;
-};
+type AllProps<ExtraProps = {}> = AppProps &
+  ExtraProps & {
+    domElement?: HTMLElement;
+    domElementGetter?(): HTMLElement;
+    // Old versions of single-spa had an appName prop
+    appName?: string;
+  };
 
 interface HelperOpts {
   domElementGetter?(): HTMLElement;
