bbb13e8894626209d2f30adcab2aca9897136add
Joel Denning <joeldenning@gmail.com>
2025-03-04 17:09:29 -0700

    Use exported types from latest single-spa

--- src/dom-element-getter-helpers.ts
+++ src/dom-element-getter-helpers.ts
@@ -1,15 +1,6 @@
-import { registerApplication, mountRootParcel } from "single-spa";
+import { CustomProps, LifeCycleFn } from "single-spa";
 
-type CustomProps = Parameters<typeof mountRootParcel>[1];
-type Application<Props extends CustomProps> = Extract<
-  Parameters<typeof registerApplication<Props>>[1],
-  { bootstrap }
->;
-type LifecycleFn<Props extends CustomProps> = Extract<
-  Application<Props>["bootstrap"],
-  (config: any) => Promise<any>
->;
-type AppProps<Props extends CustomProps> = Parameters<LifecycleFn<Props>>[0];
+type AppProps<Props extends CustomProps> = Parameters<LifeCycleFn<Props>>[0];
 
 type AllProps<ExtraProps = {}> = AppProps<ExtraProps> & {
   domElement?: HTMLElement;
